53bd782e00c364b1a5191fd6045a94b433ad2a94
mahmoudsamy <mahmoudsamy995@gmail.com>
2018-03-14 14:07:55 +0200

    initial version

---
+++ ProLink/app/src/main/java/com/example/mahmoud/prolink/tab2.java
@@ -0,0 +1,22 @@
+package com.example.mahmoud.prolink;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class tab2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2, container, false);
+
+        return rootView;
+    }
+}
---
+++ ProLink/app/src/main/java/com/example/mahmoud/prolink/tab3.java
@@ -0,0 +1,19 @@
+package com.example.mahmoud.prolink;
+
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class tab3 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab3, container, false);
+
+        return rootView;
+    }
+}
